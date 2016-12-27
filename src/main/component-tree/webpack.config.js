b5318c6ceb9815603c4dd1fd656d36b1254c2289
s.ovcharova <s.ovcharova@proxiad.com>
2016-12-27 12:27:03 +0200

    } Remove redundant code and comments

--- src/main/component-tree/webpack.config.js
+++ src/main/component-tree/webpack.config.js
@@ -3,23 +3,10 @@ var path = require('path');
 
 module.exports = {
   entry: {
-	  //the list of folders contains a single .js file to which all the components are transpiled
-    //src corresponds with the component tree src folder
-    home: './src/home.js',
-    commentsList: './src/comment/tab/comments-list.js',
-    offerSending: './src/offersending/offer-sending.js',
-    requestSearch: './src/request-search.js',
-    management: './src/management.js',
-    agentPermissions: './src/administration/agent-permissions.js',
-    validationManagement: './src/administration/validation/validation-management.js',
-    prioritization: './src/administration/prioritization/prioritization.js',
-    roleManagement: './src/administration/roles/role-management.js',
-	   payment: './src/payment/payment.js',
-    runAs: './src/run-as.js',
-    documents: './src/document/documents.js'
+	
   },
   output: {
-    //I should have a js folder in the webapp folder in order for this to work
+   
     path: '../../../src/main/webapp/js/',
     filename: '[name].js'
   },
@@ -32,7 +19,7 @@ module.exports = {
         ],
         loader: 'babel',
         query: {
-          presets: ['es2015', 'react'] //here we specify the dialect in this case the jsx and the react framework.
+          presets: ['es2015', 'react'] 
         }
       }
     ]
